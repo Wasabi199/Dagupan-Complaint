6326654b43093ea395a7ec4b17f137a0114e4fd3
RaphaelFriedChicken <leandroraphaeltenorio@gmail.com>
2022-08-07 21:10:52 +0800

    Update UserComplaint.vue

--- Dagupan-Complaint/resources/js/Pages/User/UserComplaint.vue
+++ Dagupan-Complaint/resources/js/Pages/User/UserComplaint.vue
@@ -1,7 +1,29 @@
 <template>
     <div class="flex text-2xl font-bold text-red-500">
-        <h1>Leandro Raphael</h1>
+       
     </div>
+<div>
+
+    <input v-model="message" placeholder="edit me" />
+
+    </div>
+
+    <div>Selected: {{ selected }}</div>
+
+<select v-model="selected">
+  <option disabled value="">Please select one</option>
+  <option>Lucao</option>
+  <option>Tapuac</option>
+  <option>Mukat</option>
+
+</select>
+
+<input v-model="message" placeholder="edit me">
+<br>
+
+<button class="border-solid">ENTER</button>
+
+
 </template>
 <script>
 export default {
