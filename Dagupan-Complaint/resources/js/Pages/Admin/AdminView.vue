dabdb883d5fa5c8f53d0f0d969eed9ed231f5027
RaphaelFriedChicken <leandroraphaeltenorio@gmail.com>
2022-08-24 17:48:40 +0800

    Update AdminView.vue

--- Dagupan-Complaint/resources/js/Pages/Admin/AdminView.vue
+++ Dagupan-Complaint/resources/js/Pages/Admin/AdminView.vue
@@ -1,9 +1,103 @@
 <template>
-    <AppLayout>
 
-        
+    <AppLayout>
+<span>
+
+   <!--complaint Full Screen--><div class=" border-4 border-[#005176]  w-6/12 h-3/6 float-right  mr-20 mt-40 rounded-lg">
+
+<div>
+
+                    <p class="m-2.5 ml-5 mt-5 text-[#005176] font-[arial]">Name</p>
+                    <p class="m-2.5 ml-5 text-[#005176] font-[arial]">Location</p>
+                    <p class="m-2.5 ml-5 text-[#005176] font-[arial]">Complaints:</p>
+
+   
+
+        <input type="text" placeholder="Complaints display here" class="p-3 border-2 border-[#005176] w-11/12  ml-5 mb-4 mt-4 rounded-lg">
+
+   
+
+      </div>
+        </div>
+
+
+
+            <!--Complaint Left-side--><div class="relative border-4  border-[#005176] w-1/4 h-4/5 mr-auto mt-10 ml-8 rounded-lg bg-[#fdfff] ">
+
+            <div class="p-3">
+                    <div class=""></div>
+                    <div class="border-b-2 border-[#005176] rounded-t-lg w-auto mt-1 p-3 hover:bg-gray-300 transition duration-300">
+                    <p class=" text-[#005176] font-[arial]">Name</p>
+                    <p class=" text-[#005176] font-[arial]">Location</p>
+                    <div class="ml-2.5">
+                    <p class="border-2 border-red-700 w-11/12 h-4/5  m-2.5 ml-1 p-3 rounded-lg font-[arial]">
+                        Lorem ipsum dolor sit  
+                    </p></div>
+                      
+            </div>
+
+                    <div class="pt-3"></div>
+                    <div class="border-b-2 border-[#005176] rounded-t-lg w-auto mt-1 p-3 hover:bg-gray-300 transition duration-300">
+                    <p class=" text-[#005176] font-[arial]">Name</p>
+                    <p class=" text-[#005176] font-[arial]">Location</p>
+                    <div class="ml-2.5">
+                    <p class="border-2 border-red-700 w-11/12 h-4/5  m-2.5 ml-2.5 p-3 rounded-lg font-[arial]">
+                        Lorem ipsum dolor sit 
+                    </p></div>
+                      
+            </div>
+
+                    <div class="pt-3"></div>
+                    <div class="border-b-2 border-[#005176] rounded-t-lg w-auto mt-1 p-3 hover:bg-gray-300 transition duration-300">
+                    <p class=" text-[#005176] font-[arial]">Name</p>
+                    <p class="text-[#005176] font-[arial]">Location</p>
+                    <div class="ml-2.5">
+                    <p class="border-2 border-red-700 w-11/12 h-4/5  m-2.5 ml-2.5 p-3 rounded-lg font-[arial]">
+                        Lorem ipsum dolor sit
+                    </p></div>
+                      
+            </div>
+
+                    <div class="pt-3"></div>
+                    <div class="border-b-2 border-[#005176] rounded-t-lg w-auto mt-1 p-3 hover:bg-gray-300 transition duration-300">
+                    <p class=" text-[#005176] font-[arial]">Name</p>
+                    <p class=" text-[#005176] font-[arial]">Location</p>
+                    <div class="ml-2.5">
+                    <p class="border-2 border-red-700 w-11/12 h-4/5  m-2.5 ml-2.5 p-3 rounded-lg font-[arial]">
+                        Lorem ipsum dolor sit 
+                    </p></div>
+                      
+            </div>
+
+                    <div class="pt-3"></div>
+                    <div class="border-b-2 border-[#005176] rounded-t-lg w-auto mt-1 p-3 hover:bg-gray-300 transition duration-300">
+                    <p class=" text-[#005176] font-[arial]">Name</p>
+                    <p class=" text-[#005176] font-[arial]">Location</p>
+                    <div class="ml-2.5">
+                    <p class="border-2 border-red-700 w-11/12 h-4/5  m-2.5 ml-2.5 p-3 rounded-lg font-[arial]">
+                        Lorem ipsum dolor sit  
+                    </p></div>
+            </div>
+                </div>
+                    </div>
+                      <!--end of left-complaint-->
+
+
+
+
+
+
+
+
+                   
+</span>
+
     </AppLayout>
+
+    <!--lupay aso si ryan-->
 </template>
+
+
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue';
 export default {
